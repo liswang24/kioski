d13d8a2518550d356460c2f0710194bdc82c52d2
Lisa Wang <liswang24@gmail.com>
2023-03-14 12:20:38 -0700

    Implement thanks page functionality

--- src/Routes/Thanks.jsx
+++ src/Routes/Thanks.jsx
@@ -1,6 +1,16 @@
 import * as React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Thanks() {
+  let navigate = useNavigate();
+
+  const routeToHome = () => {
+    let path = `/`
+    navigate(path);
+  }
+
+  setTimeout(routeToHome, 5000);
+
   return (
     <>
         <h1>Thanks</h1>
