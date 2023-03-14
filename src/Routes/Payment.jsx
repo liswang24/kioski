1487fd0bf5811ec3e36826efac06ab157bb4a845
Lisa Wang <liswang24@gmail.com>
2023-03-14 10:16:05 -0700

    Implement payment success

--- src/Routes/Payment.jsx
+++ src/Routes/Payment.jsx
@@ -1,23 +1,18 @@
-import { Box } from '@mui/material';
 import * as React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function Payment() {
   let navigate = useNavigate();
 
-  const routeToDispense = () => {
-      //TODO: navigate to dispense items
-    // navigate(path);
-  }
+  // On click, simulate successful payment
+  window.addEventListener('click', (event) => {
+    navigate(`../Dispense`)
+  })
 
-    return (
-      // TODO: Implement better solution than button component
-      <Box component="button" onClick={routeToDispense}>
-        <h1>Prompt Payment Here</h1>
-        {/* TODO: on tap route to next */}
-      </Box>
-    );
-  }
-  
-  export default Payment;
+  return (
+      <h1>Prompt Payment Here</h1>
+  );
+}
+
+export default Payment;
   
