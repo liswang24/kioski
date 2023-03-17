5b9bcf19aa09fa601ac5779df0d0d3286d6eaedc
Lisa Wang <liswang24@gmail.com>
2023-03-16 22:09:35 -0700

    Add product info data

--- src/productData.jsx
+++ src/productData.jsx
@@ -1,29 +1,135 @@
 export default [
     {
       id: "1",
-      name: "Grumpy Cat Poster",
-      description: "Everyone's favorite cat who loves to hate",
-      price: 15
+      name: "Chrome Dino Set",
+      brand: "Dyzplastic",
+      description: 
+        "We designed this set for Google to celebrate 10 years of the Chrome browser! If you've ever lost internet while using Chrome you may have been introduced to the pixelly prehistoric cacti-hopping, pterodactyl-dodging adventures of the Chrome Dino. Previously available exclusively within the company, we now have some sets for public sale!",
+      features: [
+        "The dinosaur figure measures 3 inches tall and about 3 inches long and 1 inch deep. Cacti vary between 2 inches and 3 inches tall",
+        "Box measures 10inches wide, 4 1/2 inches tall and 1 3/4 inches deep",
+        "Constructed of ABS plastic and acrylic",
+        "They come connected to base plates for stability, but can be removed if you want to create your own adventures"
+      ],
+      price: 24,
+      images: [
+        '../Assets/Products/chrome-dino/chrome-dino-1.png',
+        '../Assets/Products/chrome-dino/chrome-dino-2.jpg', 
+        '../Assets/Products/chrome-dino/chrome-dino-3.webp',
+        '../Assets/Products/chrome-dino/chrome-dino-4.jpg',
+        '../Assets/Products/chrome-dino/chrome-dino-5.jpg',
+        '../Assets/Products/chrome-dino/chrome-dino-6.jpg'
+      ]
     },
     {
       id: "2",
-      name: "Stretch Armstrong",
+      name: "Chrome Dino Set Dark Mode",
+      brand: "Dyzplastic",
       description:
-        "He bends! He stretches! He even ties in knots, but always returns to his original shape!",
-      price: 20
+        "If you've ever lost internet while using Chrome you may have been introduced to the pixelly prehistoric cacti-hopping pterodactyl-dodging adventures of the Chrome Dino.",
+      features: [
+        "The dinosaur figure measures 3 inches tall and about 3 inches long and 1 inch deep. Cacti vary between 2 inches and 3 inches tall",
+        "Box measures 10inches wide, 4 1/2 inches tall and 1 3/4 inches deep",
+        "Constructed of ABS plastic and acrylic",
+        "They come connected to base plates for stability, but can be removed if you want to create your own adventures"
+      ],
+      price: 24,
+      images: [
+        '../Assets/Products/chrome-dino-dark/chrome-dino-dark-1.png',
+        '../Assets/Products/chrome-dino-dark/chrome-dino-dark-2.webp', 
+        '../Assets/Products/chrome-dino-dark/chrome-dino-dark-3.webp',
+        '../Assets/Products/chrome-dino-dark/chrome-dino-dark-4.webp',
+        '../Assets/Products/chrome-dino-dark/chrome-dino-dark-5.jpg',
+        '../Assets/Products/chrome-dino-dark/chrome-dino-dark-6.jpg'
+      ]
     },
     {
       id: "3",
-      name: "Hungry Hungry Hippos Game",
+      name: "Happy Little Dinosaurs",
+      brand: "Unstable Games",
       description:
-        "It's a race, it's a chase, hurry up and feed their face! Who will win? No one knows! Feed the hungry hip-ip-pos!",
-      price: 30
+        "Lately, it feels like we’re all just dinosaurs trying to avoid the falling meteors. In this game, you’ll try to dodge all of life’s little disasters. You might fall into a pit of hot lava or get ghosted by your dino date, but the dino who survives it all wins the game!",
+      features: [
+        "2-4 players",
+        "30-60 minute playtime",
+        "Ages 6+",
+        "Expansions: 5-6 Player Expansion, Perils of Puberty Expansion, Dating Disasters Expansion"
+      ],
+      price: 22,
+      images: [
+        '../Assets/Products/happy-little-dino/hld-1.png',
+        '../Assets/Products/happy-little-dino/hld-2.png',
+        '../Assets/Products/happy-little-dino/hld-3.jpg',
+        '../Assets/Products/happy-little-dino/hld-4.jpg',
+        '../Assets/Products/happy-little-dino/hld-5.jpg',
+      ]
     },
     {
       id: "4",
-      name: "Crossfire board game",
-      description: "You'll get caught up in the crossfire!",
-      price: 35
+      name: "Mighty Dinosaurs",
+      brand: "LEGO®",
+      description:
+        "Have fun with the awesome T. rex, featuring a dark-green and beige color scheme, bright orange eyes, posable joints and head, large claws and an opening mouth with pointed teeth. Also includes the dinosaur’s prey in the form of a buildable rib cage. Rebuild this 3-in-1 LEGO® Creator Mighty Dinosaurs model to create a Triceratops or a Pterodactyl.",
+      features: [
+        "This set includes over 170 pieces and offers an age-appropriate build and play experience for ages 7-12",
+        "Rebuild to create other Mighty Dinosaurs",
+        "T. rex measures over 4” (11cm) tall",
+        "Triceratops measures over 3” (9cm) tall",
+        "Pterodactyl measures over 1” (4cm) high, 7” (18cm) long and 9” (25cm) wide"
+      ],
+      price: 30,
+      images: [
+        '../Assets/Products/mega-dino/mega-dino-1.png',
+        '../Assets/Products/mega-dino/mega-dino-2.webp',
+        '../Assets/Products/mega-dino/mega-dino-3.webp',
+        '../Assets/Products/mega-dino/mega-dino-4.webp',
+        '../Assets/Products/mega-dino/mega-dino-5.webp',
+        '../Assets/Products/mega-dino/mega-dino-6.webp'
+      ]
+    },
+    {
+      id: "5",
+      name: "Land of Dinosaurs Puzzle",
+      brand: "eeBoo",
+      description:
+        "Puzzles are a wonderful activity for young children that requires, careful observation, spacial reasoning and patience.  Piecing together a puzzle develops familiarity with pattern, shape, and color recognition. Children are challenged by the task and gifted confidence by the puzzles completion!",
+      features: [
+        "PIECE COUNT: 100 glossy pieces that fit and snap together with ease",
+        "PUZZLE SIZE is 27 x 18 inches when finished",
+        "This floor sized puzzle is rectangular in format",
+        "Perfect for ages 5 years and up and a great family activity!",
+        "SUSTAINABLY SOURCED: Made with recycled board and printed on FSC-certified paper using vegetable-based inks"
+      ],
+      price: 20,
+      images: [
+        '../Assets/Products/dino-puzzle/puzzle-1.png',
+        '../Assets/Products/dino-puzzle/puzzle-2.jpg',
+        '../Assets/Products/dino-puzzle/puzzle-3.jpg',
+        '../Assets/Products/dino-puzzle/puzzle-4.jpg',
+        '../Assets/Products/dino-puzzle/puzzle-5.jpg'
+      ]
+    },
+    {
+      id: "6",
+      name: "Inflatable Dinosour Costume",
+      brand: "Rubie's",
+      description:
+        "T-rex inflatable full-body costume(w/open face), w gloves ( sound component in gloves) w battery operated fan (batteries not included). You're never to old - or to young - to have fun dressing up!",
+      features: [
+        "100% Polyester",
+        "Zipper Closure",
+        "Hand Wash Only",
+        "Officially licensed jurassic world costume",
+        "Inflates using motor powered by 4 AA batteries (not included), pouch carries motor, suit zips in the back"
+      ],
+      price: 45,
+      images: [
+        '../Assets/Products/costume/costume-1.png',
+        '../Assets/Products/costume/costume-2.jpg',
+        '../Assets/Products/costume/costume-3.jpg',
+        '../Assets/Products/costume/costume-4.jpg',
+        '../Assets/Products/costume/costume-5.jpg'
+      ]
     }
   ];
   
