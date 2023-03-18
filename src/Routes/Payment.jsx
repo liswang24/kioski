3587d2edfe008b146991606289f426b29aa5b799
Lisa Wang <liswang24@gmail.com>
2023-03-18 13:51:05 -0700

    Implement payment UI

--- src/Routes/Payment.jsx
+++ src/Routes/Payment.jsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Typography } from '@mui/material';
+import ArrowUpwardRoundedIcon from '@mui/icons-material/ArrowUpwardRounded';
 
 function Payment() {
   let navigate = useNavigate();
@@ -14,7 +16,23 @@ function Payment() {
   })
 
   return (
-      <h1>Prompt Payment Here</h1>
+    <>
+      <ArrowUpwardRoundedIcon
+        sx={(theme) => ({
+          transform: 'scale(15)',
+          color: theme.palette.green.main,
+          marginLeft: '800px',
+          marginTop: '140px'
+        })}
+      />
+      <Typography variant='h2' m={12} mt={24}>Please complete your transaction on the terminal above.</Typography>
+      <img
+        width='70%'
+        style={{margin: '20px 160px'}}
+        src={require('../Assets/payment.png')}
+        alt='Payment device'
+      />
+    </>
   );
 }
 
